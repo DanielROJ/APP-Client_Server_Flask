e2fa9bd0fb8e211d102a9537e2b535675abcb9ab
Daniel Rojas <daniel-rojas2016@hotmail.com>
2019-05-22 02:02:52 -0500

    Se logra hacer conexion y entregar json con los datos, asi mismo se operan y se devuelven

--- Client.py
+++ Client.py
@@ -3,9 +3,10 @@
 """
 Created on Tue May 21 21:30:08 2019
 
-@author: Daniel Rojas 
+@author: Daniel Rojas - danielroj
 
-Script encargado de enviar un vector a un servidor utilizanodo el protocolo Http
+Script encargado de enviar un vector a un servidor utilizanodo el protocolo Http.
+Asi mismo se encarga de imprimir la matriz transpuesta del vector enviado que recibe del servidor
 
 """
 
@@ -14,16 +15,20 @@ import json
 import numpy as np
 
 def  Go_Client():
-    arr = np.array([],dtype=int);
+    arr = np.array([],dtype=int); ## Se unicializa un vector vacio tipo Integer
     
-    for i  in range(1,13):
+    for i  in range(1,13): ## Se llena el vector con 12 pociciones
         arr = np.append(arr,i)
         pass
-    string_arr = np.array2string(arr,separator=",")
-    js = json.dumps(string_arr)
-
-    response = req.post("http://localhost:5000/index",json=js);
-    print(response.status_code)
+    string_arr = np.array2string(arr,separator=",") 
+    js = json.dumps(string_arr) ##se convierte a json
+    print("\n Se envia el siguiete Vector ")
+    print(js + "\n")
+    response = req.post("http://localhost:5000/index",json=js); ## Se envia la peticion Http al Servidor
+    js_trans = response.json() ## Se recibe el Json respuesta del Servidor
+    
+    print("Se recibe del Servidor La siguiente Matriz")
+    print(js_trans + "\n") ## Se imprime la Respuesta del servidor
 
 
 
--- Server.py
+++ Server.py
@@ -3,31 +3,62 @@
 """
 Created on Tue May 21 21:22:04 2019
 
-@author: droro
+@author: Daniel Rojas - danielroj
+
+Script encargado de funcionar como servidor utlizando el protocolo http,
+Asi mismo, se encarga de recibir un JSON con un vector y convertirlo en una matriz transpuesta y enviarselo al cliente de origen de nuevo
+
 """
 
+## Se importan las librerias necesarias
 from flask import Flask
 
 from flask import request as req
 
 import json
 
+import numpy as np
 
-
+##Se inicializa el Servidor
 app = Flask(__name__);
 
 
+##Se establece la ruta para atender la peticion y el tipo de metodo Http
 @app.route("/index",methods=['GET', 'POST'])
 
 def index():
-    js = req.json;
+    js = req.json;    ##Recibe el json de un cliente
+    arr = json.loads(js); ## Convierte el json a String
     
-    arr = json.loads(js);
-    arr = arr.split()
-    print(arr)
+    list_l = str(arr).replace("[","").replace("]","").replace(" ","").split(",") ##Elimina Simbolos innecesarios
+    
+    numL= np.array([]);
+    for i in list_l:  ## Convierte cada simobolo a tipo Integer
+        numL=np.append(numL,int(str(i).replace(" ","")));
+        pass
+    
+    ## Se Define Tamanio de la Matriz Trnaspuesta
+    num_column=4; 
+    num_row=3;
+    a=0;
+    b= num_column;
+    matriz = [];  
+    for i in range(1,num_row +1): ##Se llena la Matriz segun los limites establecidos
+            auxL = np.array(numL[a:b]);
+            a+=4;
+            b+=4;
+            
+            matriz.append(auxL) ##Anida cada fila
+            pass
+    matriz = np.array(matriz) 
+    trans = matriz.transpose() ## Saca la transpuesta de la Matriz Creada
+    str_trans = np.array_str(trans) ## Se convierte a String
+    js_trans = json.dumps(str_trans) 
+    print(js_trans) 
+        
     
     
-    return "ok"
+    return  js_trans ## Se convierte y se retorna un JSON
     
 
 
